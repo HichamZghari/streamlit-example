3e2ae65ea2091f161f21d5e385ced49778856411
Hicham Zghari <34622132+HichamZghari@users.noreply.github.com>
2023-10-09 20:21:18 +0000

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,20 @@ import math
 import pandas as pd
 import streamlit as st
 
+# Title
 st.title("This is a title")
 
+# Header
+st.header("This is a header") 
+ 
+# Subheader
+st.subheader("This is a subheader")
+
+# Text
+st.text("Hello GeeksForGeeks!!!")
+
+# Markdown
+st.markdown("### This is a markdown")
+
+# success
+st.success("Success")
